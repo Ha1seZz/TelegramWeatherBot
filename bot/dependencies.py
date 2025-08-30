24305755b0d8eba68f0f8ec98712d81518eee563
Ha1seZz <fahretdinovdamir7@gmail.com>
2025-08-30 20:24:16 +0500

    Fix: изменить расположение БД с городами пользователей

--- bot/dependencies.py
+++ bot/dependencies.py
@@ -2,7 +2,7 @@ from utils.json_utils import JsonUtils
 from utils.logger import Logger
 
 # Создаём объект для работы с json
-user_cities_json = JsonUtils("TelegramWeatherBot/logs/user_cities.json")
+user_cities_json = JsonUtils("config/user_cities.json")
 
 # Создаём объект для работы с логами
 logs = Logger()
